3580429e7284122284099d87d6687f8ca07cb38b
ebraheem <ebraheem20012020@gmail.com>
2025-12-18 13:09:36 +0200

    [ADD] implement simpified logical condition

--- src/frontend/parsers/dml/logical_condition_parser.ts
+++ src/frontend/parsers/dml/logical_condition_parser.ts
@@ -1,16 +1,16 @@
-import { NotImplementedException } from "@nestjs/common";
 import { Parser } from "src/frontend/parser";
+import { premitive } from "src/interfaces/catalog";
 import { LogicalConditionStatement } from "src/interfaces/dml/logical_condition_ast";
 import { TokenType } from "src/interfaces/token";
 
 export class LogicalConditionParser extends Parser {
-    private static _logical_operators: string[] = ["AND", "OR", "NOT"];
-    private static _comparison_operators: string[] = ["=", "!=", ">=", "<=", ">", "<"];
-    private static _arithmetic_operators: string[] = ["+", "-", "*", "/", "%"];
-    private static _betwise_operators: string[] = ["&", "|", "#"];
-
     public parse() : LogicalConditionStatement {
         this.consume(TokenType.KEYWORD, 'WHERE');
-        throw new NotImplementedException();
+        return {
+            type: "LogicalCondition",
+            left: this.consume(TokenType.IDENTIFIER).value as string,
+            operator: this.consume(TokenType.OPERATOR).value as string,
+            right: this.consume().value as premitive
+        }
     }
 }
